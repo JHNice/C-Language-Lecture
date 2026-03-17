d232e1aead2458164cb1e0015960913c7c96d55e
JHNice <namoo5087@gmail.com>
2026-03-17 19:47:18 +0900

    2026.3.17 변수 & 상수

---
+++ Class1th Compile/Source.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// main() : 프로그램의 진입점 역할을 수행하는 함수
+void main()
+{
+	// ; (세미 콜론) : 한 문장의 끝을 알려준는 기호
+	// \n (개행 문자) : 해당하는 위치를 한 칸 아래로 내려주는 문자
+	printf("Hello world!\n");
+	printf("Input...");
+}
