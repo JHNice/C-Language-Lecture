34fa8d85a69b4d79118b8a716e2e2a966662fa44
JHNice <namoo5087@gmail.com>
2026-03-24 19:46:28 +0900

    2026.3.24 전처리기

---
+++ Class7th Preprocessor/Source.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+// #define SIZE 5
+
+void main()
+{
+#pragma region 전처리기
+	// 프로그램이 컴파일되기 이전에 프로그램에 대한 사전
+	// 처리를 하는 과정입니다.
+#pragma region 매크로
+	// 프로그램 내에서 특정한 데이터가 문자열로 정의되고
+	// 처리되는 과정입니다.
+
+	// printf("SIZE : %d\n", SIZE);
+
+	// 매크로의 경우 자료형이 존재하지 않으므로, 메모리 공간을
+	// 가지고 있지 않습니다.
+
+	// 매크로의 과정은 컴파일 이전에 실행되며, 각 매크로가 실행될 때
+	// 매크로의 대체 목록을 넣어야 하므로 프로그램의 크기가 커지게 됩니다.
+
+
+
+#pragma endregion
+
+
+#pragma endregion
+#pragma region 단축 평가 계산
+	// 계산을 진행하는 도중에 결과가 이미 확정된 경우
+	// 나머지 계산을 생략하는 과정입니다.
+
+	// int x = 0;
+	// int y = 0;
+	// 
+	// if (x != y && y++)
+	// {
+	// 	printf("&& 결과\nx : %d\ny : %d\n", x, y);
+	// 
+	// }
+	// printf("&& 이후 \nx : %d\ny : %d\n", x, y);
+	// if (x != y || y++)
+	// {
+	// 	printf("|| 결과\nx : %d\ny : %d\n", x, y);
+	// 
+	// }
+	// printf("|| 이후 \nx : %d\ny : %d\n", x, y);
+
+	// &&연산자의 경우 둘중 하나라도 거짓이면 안 되기 때문에 처음 x != y 가 거짓이므로 뒤에 y++을 수행하지 않음
+	// ||연산자의 경우 둘중 하나만 참이면 되기때문에 y++을 수행하긴함
+
+#pragma endregion
+#pragma region 자료형 변환
+	// 서로 다른 자료형을 가지고 있는 변수끼리 연산이
+	// 이루어질 때 기존에 지정했던 자료형을 다른 자료형
+	// 으로 변환하는 과정입니다.
+#pragma region 암묵적 형 변환
+	// 서로 다른 자료형으로 연산이 이루어질 때 자료형의
+	// 크기가 더 큰 자료형으로 변환되는 과정입니다.
+
+	// int defense = 10;
+	// float endurance = 3.5f;
+	// 
+	// printf("defense + endurance : %f\n", defense + endurance);
+	// 
+	// int a = 10;
+	// float b = 3.5f;
+	// a = b;
+	// printf("%d", a);
+
+	// 표현 범위가 작은 데이터에 표현 범위가 큰 데이터를
+	// 저장하게 되면 암묵적으로 데이터 손실이 발생합니다.
+
+#pragma endregion
+#pragma region 명시적 형 변환
+	// 연산이 이루어지기 전에 사용자가 직접 자료형을
+	// 변환하는 과정입니다.
+
+	// int a = 10;
+	// int b = 3;
+	// float c = (float)a / b;
+	// printf("f : %f\n", c);
+
+	// 정수형 변수끼리 연산을 수행하게 되면 정수의 결과
+	// 값만 가질 수 있습니다.
+
+#pragma endregion
+#pragma endregion
+
+
+
+}
